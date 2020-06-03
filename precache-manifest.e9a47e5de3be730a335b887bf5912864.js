d47ec60c70dd81a5e867cbc6b8b2f7e0b856bb2c
Nedra Krifa <nedrakrifa@gmail.com>
2020-06-03 18:45:14 +0200

    Updates

---
+++ precache-manifest.e9a47e5de3be730a335b887bf5912864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ab36f644b63a295f15e350df0309e7",
+    "url": "/react-stars/index.html"
+  },
+  {
+    "revision": "a082b1d318c978427ce5",
+    "url": "/react-stars/static/css/main.5849604f.chunk.css"
+  },
+  {
+    "revision": "c343e10cd4fc38369124",
+    "url": "/react-stars/static/js/2.b5f8e02b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stars/static/js/2.b5f8e02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a082b1d318c978427ce5",
+    "url": "/react-stars/static/js/main.060ec568.chunk.js"
+  },
+  {
+    "revision": "889ea61aa0aa4f545c55",
+    "url": "/react-stars/static/js/runtime-main.843aacd1.js"
+  }
+]);
